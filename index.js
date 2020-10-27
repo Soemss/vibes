698caf6e1ca6311b4e43126793a031471d989d85
Soems <email>
2020-10-26 21:32:48 -0700

    config fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const config = require('./config')
+const config = require('./config.json')
 const command = require('./command')
 
 client.on('ready', () => {
