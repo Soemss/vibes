69b14fe369696be1fc036657ad7d315b0308326d
Soems <email>
2020-10-27 18:29:58 -0700

    dotenv require

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const config = require('./config.json')
 const command = require('./command')
+require('dotenv').config();
 
 client.on('ready', () => {
     console.log('The client is ready')
