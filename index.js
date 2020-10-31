81265da57b9b11686549b823dce23c145878fcd1
Soems <email>
2020-10-31 02:09:33 -0700

    Rollback

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const command = require('./command')
 
 client.on('ready', () => {
     console.log('The client is ready')
-    command(client, ['ping', 'test'], (message) => {
-        message.channel.send('Pong!')
-    })
 })
 
-console.log(client.login(process.env.TOKEN))
+client.login(process.env.TOKEN);
