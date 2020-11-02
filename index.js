fc68144fae2aa421ec7b028c86bf1ced2b43fdc0
Soems <email>
2020-11-01 20:47:36 -0800

    command file

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
+const config = require('./config.json')
+const command = require('./command')
+
+require('dotenv').config()
+
 client.on('ready', () => {
-    console.log('The client is ready')
+    console.log('Music go brrr')
+
+    command(client, 'ping', message => {
+        message.channel.send('Pong!')
+    })
 })
 
 client.login(process.env.TOKEN);
