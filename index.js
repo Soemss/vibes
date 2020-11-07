abc4b70e354ccf95d533ea83de3e095863e9f5f6
Soems <email>
2020-11-07 00:47:55 -0800

    command file fix

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-require('dotenv').config()
+const queue = new Map()
 
 client.on('ready', () => {
     console.log('Music go brrr')
      
 })
 
-client.on("message", message => {
+client.on("message", async(message) => {
     const prefix = '<'
+
+    const serverQueue = queue.get(message.guild.id)
+
     const args = message.content.slice(prefix.length).trim().split(/ +/g)
     const command = args.shift().toLowerCase()
 
     if(command === 'play') {
-        console.log("Now playing " + args[0])
+        execute(message, serverQueue)
+    }
+
+    async function execute(message, serverQueue) {
+        let vc = message.member.voice.channel;
+        if(!vc) {
+            return message.channel.send("You are not in a voice chat.")
+        
+        } else {
+            //
+        }
     }
 })
 
